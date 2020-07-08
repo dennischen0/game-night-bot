02cca2e6fdc431642082f093a50ddc8b5727cf83
Dennis Chen <c.w.dennis@gmail.com>
2020-07-07 22:19:21 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# discord-hello-world-bot
---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+    msg.channel.send('pong');
+
+  } else if (msg.content.startsWith('!kick')) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+      console.info(taggedUser.username.toLowerCase());
+      if (taggedUser.username.toLowerCase() === "dennis") {
+        console.info("Test");
+        
+        msg.channel.send(`${taggedUser.username} cannot be kicked.`);
+      }
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  }
+});
