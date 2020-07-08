0d656badda2760fa4d0af3d5ebf1ba2cc4e5306f
Dennis Chen <c.w.dennis@gmail.com>
2020-07-08 12:25:57 -0700

    Add promotion/demotion

--- index.js
+++ index.js
@@ -19,9 +19,24 @@ bot.on('message', msg => {
       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
       console.info(taggedUser.username.toLowerCase());
       if (taggedUser.username.toLowerCase() === "dennis") {
-        console.info("Test");
-        
         msg.channel.send(`${taggedUser.username} cannot be kicked.`);
+      }
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  } else if (msg.content.startsWith('!promote') || msg.content.startsWith('!demote')) {
+    let roleCache = msg.guild.roles.cache
+    let promotedRole = roleCache.find(role => role.name === "Promoted");
+    let demotedRole = roleCache.find(role => role.name === "Demoted");
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      let guildMember = msg.guild.member(taggedUser);
+      if (msg.content.startsWith('!promote')) {
+        guildMember.roles.add(promotedRole);
+        guildMember.roles.remove(demotedRole);
+      } else {
+        guildMember.roles.add(demotedRole);
+        guildMember.roles.remove(promotedRole);
       }
     } else {
       msg.reply('Please tag a valid user!');
