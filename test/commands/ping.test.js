14202c8ee70648e0161cae1b7131aedcbc62d888
Dennis Chen <c.w.dennis@gmail.com>
2020-07-13 00:56:00 -0700

    Add jest for unit testing

---
+++ test/commands/ping.test.js
@@ -0,0 +1,11 @@
+const ping = require('../../commands/ping.js')
+
+describe('ping', () => {
+  const mockMessage = {
+    reply: jest.fn(() => {})
+  }
+  test('should respond with pong', () => {
+    ping.execute(mockMessage)
+    expect(mockMessage.reply).toHaveBeenCalledWith('pong')
+  })
+})
