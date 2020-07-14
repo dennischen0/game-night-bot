b961f3ea17a87a19a43beb3d01355785be5037d1
Dennis Chen <c.w.dennis@gmail.com>
2020-07-13 19:20:40 -0700

    Remove args-info usage

--- src/commands/args-info.js
+++ src/commands/args-info.js
@@ -2,7 +2,6 @@ module.exports = {
   name: 'args-info',
   description: 'Information about the arguments provided.',
   args: true,
-  usage: '<user> <role>',
   execute (message, args) {
     if (args[0] === 'foo') {
       return message.channel.send('bar')
