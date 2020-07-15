68d9002feaa72b21033d48806b98e83422cd7326
Dennis Chen <c.w.dennis@gmail.com>
2020-07-14 19:27:19 -0700

    Test avatar

---
+++ test/commands/avatar.test.js
@@ -0,0 +1,19 @@
+const avatar = require('../../src/commands/avatar.js')
+
+describe('when no user mentioned', () => {
+  const message = {
+    channel: {
+      send: jest.fn(() => {})
+    },
+    mentions: {
+      users: []
+    },
+    author: {
+      displayAvatarURL: jest.fn(() => { return 'https://avatar.png' })
+    }
+  }
+  test('should respond with user\'s avatar', () => {
+    avatar.execute(message)
+    expect(message.channel.send).toHaveBeenCalledWith(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`)
+  })
+})
